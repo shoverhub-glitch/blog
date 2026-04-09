a28b52466ad4a614e5da28374b3f8c88d19f89c9
Rakesh Yensi <rakesh@aiyensi.com>
2026-04-09 09:57:19 +0530

    deleted old ui

--- src/config.ts
+++
@@ -1,5 +0,0 @@
-// Use official AdSense test ID if no publisher ID is provided
-const TEST_ID = 'ca-pub-3940256099942544';
-export const config = {
-  adsensePublisherId: import.meta.env.VITE_ADSENSE_PUBLISHER_ID || TEST_ID,
-};
